4a30e6d2cf82e9b76d7c7cf1de7076d464b6339b
karenvaleria-r <karenvaleria2208@icloud.com>
2024-04-22 16:14:30 -0400

    tarealista

--- task3.js
+++ task3.js
@@ -1,8 +1,12 @@
 export class Player {
-  constructor() {
-  
-  }
-
-
+  constructor(name,level) {
+  this.name = name
+  this.level = level
+  this.info = function(){
+    console.log(this.name + " has reached Level "+ this.level +"!" )
+}
+}
 
 }
+const taraPlayer = new Player(`Tara`, 6)
+console.log(taraPlayer.info())
